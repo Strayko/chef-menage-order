1ef85065262d514f23c8dd262b99eaaf8bc80e35
Strayko <strayko_lol@windowslive.com>
2020-06-01 15:44:05 +0200

    create migration and add category

--- Taste.DataAccess/Data/Repository/IRepository/ICategoryRepository.cs
+++ Taste.DataAccess/Data/Repository/IRepository/ICategoryRepository.cs
@@ -1,10 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Taste.Models;
 
 namespace Taste.DataAccess.Data.Repository.IRepository
 {
-    interface ICategoryRepository
+    public interface ICategoryRepository : IRepository<Category>
     {
+        IEnumerable<SelectListItem> GetCategoryListForDropDown();
+
+        void Update(Category category);
     }
 }
