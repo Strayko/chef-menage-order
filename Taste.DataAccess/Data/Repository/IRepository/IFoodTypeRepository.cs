ed60b3527b5148262309748dd18a99ef3fab6c11
Strayko <strayko_lol@windowslive.com>
2020-06-05 16:28:49 +0200

    FoodType Controller and API (create, update and delete)

---
+++ Taste.DataAccess/Data/Repository/IRepository/IFoodTypeRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Taste.Models;
+
+namespace Taste.DataAccess.Data.Repository.IRepository
+{
+    public interface IFoodTypeRepository : IRepository<FoodType>
+    {
+        IEnumerable<SelectListItem> GetFoodTypeListForDropDown();
+
+        void Update(FoodType foodType);
+    }
+}
