36c97be7c439bbadabc9f829182faf6fbd2302a1
Strayko <strayko_lol@windowslive.com>
2020-06-08 17:10:39 +0200

    Menu Item in process

--- Taste.DataAccess/Data/Repository/UnitOfWork.cs
+++ Taste.DataAccess/Data/Repository/UnitOfWork.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using Taste.DataAccess.Data.Repository.IRepository;
+using Taste.Models;
 
 namespace Taste.DataAccess.Data.Repository
 {
@@ -14,11 +15,14 @@ namespace Taste.DataAccess.Data.Repository
             _db = db;
             Category = new CategoryRepository(_db);
             FoodType = new FoodTypeRepository(_db);
+            MenuItem = new MenuItemRepository(_db);
         }
 
         public ICategoryRepository Category { get; private set; }
 
         public IFoodTypeRepository FoodType { get; private set; }
+
+        public IMenuItemRepository MenuItem { get; private set; }
 
         public void Dispose()
         {
---
+++ Taste.Models/ViewModels/MenuItemVM.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Taste.Models
+{
+    public class MenuItemVM
+    {
+        public MenuItem MenuItem { get; set; }
+        public IEnumerable<SelectListItem> CategoryList { get; set; }
+        public IEnumerable<SelectListItem> FoodTypeList { get; set; }
+    }
+}
