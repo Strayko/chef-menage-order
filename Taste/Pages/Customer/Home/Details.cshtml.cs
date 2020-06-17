b921dc3c24605735d1b1c8b43150d557f85deafb
Strayko <strayko_lol@windowslive.com>
2020-06-17 14:51:49 +0200

    Shopping Cart Session

--- Taste/Pages/Customer/Home/Details.cshtml.cs
+++ Taste/Pages/Customer/Home/Details.cshtml.cs
@@ -4,10 +4,12 @@ using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Taste.DataAccess.Data.Repository.IRepository;
 using Taste.Models;
+using Taste.Utility;
 
 namespace Taste.Pages.Customer.Home
 {
@@ -51,6 +53,22 @@ namespace Taste.Pages.Customer.Home
                 {
                     _unitOfWork.ShoppingCart.Add(ShoppingCartObj);
                 }
+                else
+                {
+                    _unitOfWork.ShoppingCart.IncrementCount(cartFromDb, ShoppingCartObj.Count);
+                }
+                _unitOfWork.Save();
+
+                var count = _unitOfWork.ShoppingCart
+                    .GetAll(c => c.ApplicationUserId == ShoppingCartObj.ApplicationUserId).ToList().Count;
+                HttpContext.Session.SetInt32(SD.ShoppingCart, count);
+                return RedirectToPage("Index");
+            }
+            else
+            {
+                ShoppingCartObj.MenuItem = _unitOfWork.MenuItem.GetFirstOrDefault(
+                    includeProperties: "Category,FoodType", filter: c => c.Id == ShoppingCartObj.MenuItemId);
+                return Page();
             }
         }
     }
