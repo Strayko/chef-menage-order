2edcfc37351e516a765fddc76fc9ef5fd86b63e8
Strayko <strayko_lol@windowslive.com>
2020-06-29 14:33:34 +0200

    Manage Order in process

--- Taste/wwwroot/js/orderList.js
+++ Taste/wwwroot/js/orderList.js
@@ -1,13 +1,22 @@
 ﻿let dataTable;
 
 $(document).ready(function () {
-    loadList();
+    let url = window.location.search;
+    if (url.includes("cancelled")) {
+        loadList("cancelled");
+    } else {
+        if (url.includes("completed")) {
+            loadList("completed");
+        } else {
+            loadList("");
+        }
+    }
 });
 
-function loadList() {
+function loadList(param) {
     dataTable = $('#DT_load').DataTable({
         "ajax": {
-            "url": "/api/order",
+            "url": "/api/order?status=" + param,
             "type": "GET",
             "datatype": "json"
         },
@@ -17,7 +26,7 @@ function loadList() {
             { "data": "orderHeader.orderTotal", "width": "20%" },
             { "data": "orderHeader.applicationUser.email", "width": "20%" },
             {
-                "data": "id",
+                "data": "orderHeader.id",
                 "render": function (data) {
                     return `<div class="text-center">
                         <a href="/Admin/Order/OrderDetails?id=${data}" class="btn btn-success text-white" style="cursor: pointer; width: 50%;">
