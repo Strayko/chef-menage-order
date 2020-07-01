ae617b5342b0958a3bd03b533df379011bec1507
Strayko <strayko_lol@windowslive.com>
2020-07-01 16:58:07 +0200

    Login Issue and End Point Routing

--- Taste/Pages/Customer/Cart/Index.cshtml.cs
+++ Taste/Pages/Customer/Cart/Index.cshtml.cs
@@ -28,7 +28,8 @@ namespace Taste.Pages.Customer.Cart
         {
             OrderDetailsCartVM = new OrderDetailsCart()
             {
-                OrderHeader = new Models.OrderHeader()
+                OrderHeader = new Models.OrderHeader(),
+                ListCart = new List<ShoppingCart>()
             };
 
             OrderDetailsCartVM.OrderHeader.OrderTotal = 0;
@@ -36,17 +37,21 @@ namespace Taste.Pages.Customer.Cart
             var claimsIdentity = (ClaimsIdentity)User.Identity;
             var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
 
-            IEnumerable<ShoppingCart> cart = _unitOfWork.ShoppingCart.GetAll(c => c.ApplicationUserId == claim.Value);
-
-            if (cart != null)
+            if (claim != null)
             {
-                OrderDetailsCartVM.ListCart = cart.ToList();
-            }
+                IEnumerable<ShoppingCart> cart =
+                    _unitOfWork.ShoppingCart.GetAll(c => c.ApplicationUserId == claim.Value);
 
-            foreach (var cartList in OrderDetailsCartVM.ListCart)
-            {
-                cartList.MenuItem = _unitOfWork.MenuItem.GetFirstOrDefault(m => m.Id == cartList.MenuItemId);
-                OrderDetailsCartVM.OrderHeader.OrderTotal += (cartList.MenuItem.Price * cartList.Count);
+                if (cart != null)
+                {
+                    OrderDetailsCartVM.ListCart = cart.ToList();
+                }
+
+                foreach (var cartList in OrderDetailsCartVM.ListCart)
+                {
+                    cartList.MenuItem = _unitOfWork.MenuItem.GetFirstOrDefault(m => m.Id == cartList.MenuItemId);
+                    OrderDetailsCartVM.OrderHeader.OrderTotal += (cartList.MenuItem.Price * cartList.Count);
+                }
             }
         }
 
--- Taste/Startup.cs
+++ Taste/Startup.cs
@@ -42,9 +42,10 @@ namespace Taste
 
             services.AddScoped<IUnitOfWork, UnitOfWork>();
 
-            services.AddMvc(options => options.EnableEndpointRouting = false)
-                .SetCompatibilityVersion(Microsoft.AspNetCore.Mvc.CompatibilityVersion.Version_3_0);
+            //services.AddMvc(options => options.EnableEndpointRouting = false)
+            //.SetCompatibilityVersion(Microsoft.AspNetCore.Mvc.CompatibilityVersion.Version_3_0);
 
+            services.AddRazorPages();
             services.AddControllersWithViews().AddRazorRuntimeCompilation();
             services.AddAuthentication().AddFacebook(facebookOptions =>
             {
@@ -99,10 +100,16 @@ namespace Taste
             app.UseStaticFiles();
             app.UseSession();
 
+            app.UseRouting();
             app.UseAuthentication();
             app.UseAuthorization();
 
-            app.UseMvc();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+                endpoints.MapRazorPages();
+            });
+            //app.UseMvc();
 
             StripeConfiguration.ApiKey = Configuration.GetSection("Stripe")["SecretKey"];
         }
